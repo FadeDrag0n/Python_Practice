68656dead143da41e7b79df5a2e9730ee24d542f
FadeDrag0n <fadedragon603@gmail.com>
2026-03-31 14:18:23 +0300

    os practice 2 - done

--- Python_Practice/os_practice/os_practice2.py
+++ Python_Practice/os_practice/os_practice2.py
@@ -1,12 +1,25 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv('test.env')
 
 # переменные окружения — это будет в каждом FastAPI проекте
-os.getenv('DATABASE_URL', 'default_value')
+print(os.getenv('DATABASE_URL', 'default_value'))
 os.environ['MY_VAR'] = 'value'
+debug = os.getenv('DEBUG', 'False')
+print(debug)
 
 # работа с путями — очень часто используется
-os.path.join('folder', 'subfolder', 'file.txt')  # кроссплатформенный путь
-os.path.abspath('file.txt')  # абсолютный путь
+os.path.join('folder', 'subfolder', 'file.txt')  # кроссплатформенный путь # Windows: folder\subfolder\file.txt
+# Linux/Mac: folder/subfolder/file.txt
+os.path.abspath('file.txt')  # абсолютный путь # → /home/fade/projects/myapp/file.txt
 os.path.dirname('/home/user/file.txt')  # → /home/user
 os.path.basename('/home/user/file.txt')  # → file.txt
 os.path.splitext('file.txt')  # → ('file', '.txt')
+
+name, ext = os.path.splitext('photo.jpg')
+if ext in ('.jpg', '.png', '.webp'):
+    print('это картинка')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
